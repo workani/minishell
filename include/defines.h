6d25ba7d9c01d1a8aa66b6df2cf1352126740b65
workani <tomdanil68@gmail.com>
2025-10-24 15:39:10 +0200

    Fix input prompt

--- include/defines.h
+++ include/defines.h
@@ -7,7 +7,7 @@
 
 # define DISCARD -1
 
-#define PROMPT "\033[48;5;23;38;5;252m >>> \033[0m\033[38;5;201m minishell$\033[0m "
+#define PROMPT "\001\033[48;5;23;38;5;252m >>> \033[0m\033[38;5;201m minishell$\033[0m \002"
 
 #define SUCCESS 0
 #define FAILURE 1
