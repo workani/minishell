7b0bb30e5675718c11029a28502b8cda5e772818
workani <tomdanil68@gmail.com>
2025-10-24 23:11:49 +0200

    Improve input prompt

--- include/defines.h
+++ include/defines.h
@@ -7,8 +7,7 @@
 
 # define DISCARD -1
 
-#define PROMPT "\001\033[48;5;23;38;5;252m >>> \033[0m\033[38;5;201m minishell$\033[0m \002"
-
+#define PROMPT "\001\033[48;5;23;38;5;252m\002 >>> \001\033[0m\033[38;5;201m\002 minishell$ \001\033[0m\002"
 #define SUCCESS 0
 #define FAILURE 1
 
